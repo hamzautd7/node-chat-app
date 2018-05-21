5e84a02261c8378046f53c0f58f262b09a302183
Hamzautd7 <hamza.utd7@gmail.com>
2018-05-21 19:40:10 +0500

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket =io();
+socket.on('connect', function(){
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+        text:'hello'
+    });
+});
+socket.on('disconnect', function(){
+    console.log('disconnected from server ');
+});
+
+socket.on('newMessage', function(message){
+    console.log('New Message');
+    console.log(JSON.stringify(message, undefined, 2))
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath= path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
+const server = app.listen(3000, ()=>{
+    console.log('Server is up');
+});
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    
+    socket.emit('newMessage', {
+        from: 'Mike@abc.com',
+        text: 'yo',
+        createdAt: 'Date'
+    });
+
+    socket.on('createMessage', (newMessage)=>{
+        console.log('createMessage: ',newMessage);
+    })
+    
+    socket.on('disconnect', ()=>{
+        console.log('disconnected from client');
+    });
+
+
+});
+
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
-   console.log('Server is up at port 3000'); 
-}); 
