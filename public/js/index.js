a34d9900dc7e513cacc80e5bf0f21f533d0785e1
Hamzautd7 <hamza.utd7@gmail.com>
2018-05-25 03:50:27 +0500

    added css

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,7 @@ socket.on('newMessage', function(message){
     li.text(`${message.from}: ${message.text}`)
     console.log(`${message.from}: ${message.text}`);
     jQuery('#messages').append(li);
+ 
 });
 
 socket.on('newLocationMessage', function(message){
@@ -28,11 +29,12 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#messageForm').on('submit', function(e){
     e.preventDefault();
+    var messageTextBox = jQuery('[name = message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name = message]').val()
+        text: messageTextBox.val()
     }, function(){
-
+        messageTextBox.val('');
     });
 });
 
@@ -41,32 +43,15 @@ locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by Browser');
     }
-
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
     navigator.geolocation.getCurrentPosition(function(position){
-       
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
         });
     },function(){
         alert('Location not found');
+        locationButton.removeAttr('disabled').text('Send Location');
     });
 });
-// const locationButton = jQuery('#sendLocation');
-// locationButton.on('click', () => {
- 
-//  if (!navigator.geolocation) {
-//  return alert('Geo location not supported by your browser');
-//  }
-//  navigator.geolocation.getCurrentPosition(function () {}, function () {}, {});
-//  navigator.geolocation.getCurrentPosition((position) => {
-//  console.log('ay');
-//  console.log(position);
-//  }, (err) => {
-//  alert('Unable to fetch location');
-//  }, {
-//  maximumAge: 60000,
-//  timeout: 10000,
-//  enableHighAccuracy: true
-//  });
-// });
