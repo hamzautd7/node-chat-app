cb627e66d4f5520c74f6f2d5da58d72952a87c69
Hamzautd7 <hamza.utd7@gmail.com>
2018-05-25 02:58:09 +0500

    added geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -13,12 +13,21 @@ socket.on('newMessage', function(message){
     console.log(JSON.stringify(message, undefined, 2))
     var li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`)
+    console.log(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My Current Location<a/>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
     jQuery('#messages').append(li);
 });
 
 jQuery('#messageForm').on('submit', function(e){
     e.preventDefault();
-    
     socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name = message]').val()
@@ -26,3 +35,38 @@ jQuery('#messageForm').on('submit', function(e){
 
     });
 });
+
+var locationButton = jQuery('#sendLocation');
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by Browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+       
+        socket.emit('createLocationMessage', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+        });
+    },function(){
+        alert('Location not found');
+    });
+});
+// const locationButton = jQuery('#sendLocation');
+// locationButton.on('click', () => {
+ 
+//  if (!navigator.geolocation) {
+//  return alert('Geo location not supported by your browser');
+//  }
+//  navigator.geolocation.getCurrentPosition(function () {}, function () {}, {});
+//  navigator.geolocation.getCurrentPosition((position) => {
+//  console.log('ay');
+//  console.log(position);
+//  }, (err) => {
+//  alert('Unable to fetch location');
+//  }, {
+//  maximumAge: 60000,
+//  timeout: 10000,
+//  enableHighAccuracy: true
+//  });
+// });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath= path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -34,7 +34,14 @@ io.on('connection', (socket)=>{
         // });
     
     });
-    
+
+    socket.on('createLocationMessage', (coords)=>{
+       // console.log(`${coords.latitude}, ${coords.longitude}`);
+        io.emit('newLocationMessage', generateLocationMessage('Admin',
+    coords.latitude, coords.longitude));
+   
+});
+   
     socket.on('disconnect', ()=>{
         console.log('disconnected from client');
     });
